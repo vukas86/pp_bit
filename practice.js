84d353ddda70b2052f5727ecfd2b72c96211bc76
BIT Student <classroom@bgit.rs>
2021-05-03 12:02:38 +0200

    initial commit

---
+++ practice.js
@@ -0,0 +1,183 @@
+/*var newBox;
+var firstPlay;
+var secondGame;
+console.log(firstplay);
+console.log(newbox);
+console.log(secondgame);*/
+
+
+/*var mood = "happy";
+console.log(mood);*/
+
+/*var numberOperations = 3 * 2 / 14;
+console.log(numberSix);*/
+
+/*var bananas = 1.2;
+var weight = 1000;
+
+var bananaWeight = bananas * weight;
+
+console.log(bananaWeight + " grams");*/
+
+
+/*var anaLate = 5 * 60;
+console.log("Ana is late " + anaLate + " extra seconds");
+
+var tomLate = 30 * 60;
+console.log("Tom is late " + tomLate + " extra seconds");
+
+var usbStick = 4;
+var gbInKb = usbStick * 1000;
+console.log(gbInKb);
+var newsArticle = gbInKb / 98;
+console.log(newsArticle);
+
+
+var a = 3;
+a *= 3;
+console.log(a);
+var b = 3;
+b += 3;
+console.log(b);
+var c = 5;
+c /= 3;
+console.log(c);
+var d = 15;
+c -= 8;
+console.log(d);
+
+var myName = "Aleksandar Vukasovic";
+var myAge = 34;
+var myCat = false;
+
+console.log(typeof (myName));
+console.log(typeof (myAge));
+console.log(typeof (myCat));
+
+console.log(typeof "number");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof "false");
+console.log(typeof 56);
+
+console.log(!!5);
+console.log(!!true);
+console.log(!!NaN);
+console.log(!!" ");*/
+
+
+/*var userAge = 150;
+if (userAge < 0 || userAge > 120) {
+    console.log("age is not correct!")
+}*/
+
+/*var a = -5;
+var b = 10;
+var c = 10;
+var d = 10;
+var e = -1;
+
+if (a >= b && a >= c && a >= d && a >= e) {
+    console.log(a);
+} else if (b >= a && b >= c && b >= d && d >= e) {
+    console.log(b);
+} else if (c >= a && b >= c && c >= d && d >= e) {
+    console.log(c);
+} else if (d >= a && d >= c && d >= d && d >= e) {
+    console.log(d);
+} else {
+    console.log(e)
+}*/
+
+/*var a = 4;
+var b = 10;
+var c = 10;
+
+if (a >= b && a >= c) {
+    if (b > c) {
+        console.log(a + ", " + b + ", " + c);
+    } else {
+        console.log(a + ", " + c + ", " + b);
+    }
+} else if (b >= a && b >= c) {
+    if (a > c) {
+        console.log(b + ", " + a + ", " + c);
+    } else {
+        console.log(b + ", " + c + ", " + a);
+    }
+
+} else if (c >= a && c >= b) {
+    if (a > b) {
+        console.log(c + ", " + a + ", " + b)
+    } else {
+        console.log(c + ", " + b + ", " + a)
+    }
+} else {
+    console.log("greska");
+}
+
+
+
+var t = 30;
+var unit = "c";
+
+
+
+if (unit === "c") {
+    var c = (9 * t / 5) + 32;
+    console.log(t + " C is " + c + " F")
+} else if (unit === "f") {
+    var f = (t - 32) / 1.8;
+    console.log(t + " F is " + f + " C");
+} else {
+    console.log("input is incorrect!")
+}*/
+
+
+
+
+/*var a = 11;
+if (a < 13) {
+    b = 13 - a;
+    console.log(b);
+}
+else if (a > 13) {
+    b = 13 - a;
+    c = b * 2;
+    console.log(c);
+}*/
+
+/*var a = 8;
+var b = 8;
+var result = a + b;
+
+if (a === b) {
+    c = result * 3;
+    console.log(c);
+} else {
+    console.log(result);
+}*/
+
+/*var a = 50;
+var b = 50;
+var result = a + b;
+if (a === 50 || result === 50) {
+    console.log('true');
+} else {
+    console.log("-");
+}*/
+
+/*var a = 256;
+
+if (a > 20 && a < 100) {
+    console.log("20 <> 100");
+} else if (a > 100 && a < 400) {
+    console.log("100 <> 400");
+} else {
+    console.log("-");
+}*/
+
+
+
+
+
