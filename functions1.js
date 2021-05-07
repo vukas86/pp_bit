0c313a1b76559560b427e08f60214279b2b25999
“vukas86” <vukasbit@gmail.com>
2021-05-07 08:45:41 +0200

    funct.1 practice

---
+++ functions1.js
@@ -0,0 +1,77 @@
+/*1.Write a program that calculates the maximum of two given numbers. 
+A = 5 b = 6 output: 6*/
+function numBer(a, b){
+	if(a>b){
+		return a;
+	}else if (b>a){
+	return b;
+}else{
+	return "the numbers are equal!";
+}
+}
+
+console.log(numBer(5,6));
+
+/*2.Write a program that checks if a given number is odd.
+A = 3 output: odd*/
+
+function oddNum (a){
+    if (a%2===1)
+    return a;
+}
+
+console.log(oddNum(3));
+
+/*3.Write a program that checks if a given number is a three digit long number.*/
+
+function digiT (n){
+    if (n>99 && n<1000)
+    return n;
+}
+
+console.log(digiT(356));
+
+/*4.Write a program that calculates an arithmetic mean of four numbers.*/
+
+var avvar="";
+function ariTM(arr){
+    var sum = 0;
+    for(var i=1; 1<arr.length; i++){
+        sum = sum + arr[i];
+        avvar = sum / arr.length;
+        return sum;
+    }
+    
+}
+
+console.log(ariTM([3,4,5,6]));
+
+/*7.Write a program that calculates a number of digits of a given number. */
+
+/*9.Write a program that calculates the sum of odd elements of a given array.*/
+function sumNum (arr){
+    var a = 0;
+    for(var i = 0; i<arr.length; i++){
+        if(i%2===1){
+            a+=arr[i];
+           
+        }
+    } return a;
+}
+
+console.log (sumNum([2,3,1,4]));
+
+/*10.Write a program that calculates a number of appearances of a given number in a given array.*/
+
+function countAp (a, e){
+    var b= 0;
+    for(var i = 0; i<a.length; i++){
+if(e===a[i]){
+    b++;
+   
+}
+return b;
+
+    }
+}
+console.log(countAp([2, 4, 7, 8, 7, 7, 1], 7));
