f7015157860e530eeb1f175b84e81a03fcfc405a
“BIT <vukasbit@gmail.com>
2021-05-07 13:08:50 +0200

    functions practice

--- functions1.js
+++ functions1.js
@@ -1,77 +1,79 @@
-/*1.Write a program that calculates the maximum of two given numbers. 
-A = 5 b = 6 output: 6*/
-function numBer(a, b){
-	if(a>b){
-		return a;
-	}else if (b>a){
-	return b;
-}else{
-	return "the numbers are equal!";
-}
-}
+// /*1.Write a program that calculates the maximum of two given numbers.
+// A = 5 b = 6 output: 6*/
+// function numBer(a, b) {
+//     if (a > b) {
+//         return a;
+//     } else if (b > a) {
+//         return b;
+//     } else {
+//         return "the numbers are equal!";
+//     }
+// }
 
-console.log(numBer(5,6));
+// console.log(numBer(5, 6));
 
-/*2.Write a program that checks if a given number is odd.
-A = 3 output: odd*/
+// /*2.Write a program that checks if a given number is odd.
+// A = 3 output: odd*/
 
-function oddNum (a){
-    if (a%2===1)
-    return a;
-}
+// function oddNum(a) {
+//     if (a % 2 === 1)
+//         return a;
+// }
 
-console.log(oddNum(3));
+// console.log(oddNum(3));
 
-/*3.Write a program that checks if a given number is a three digit long number.*/
+// /*3.Write a program that checks if a given number is a three digit long number.*/
 
-function digiT (n){
-    if (n>99 && n<1000)
-    return n;
-}
+// function digiT(n) {
+//     if (n > 99 && n < 1000)
+//         return n;
+// }
 
-console.log(digiT(356));
+// console.log(digiT(356));
 
-/*4.Write a program that calculates an arithmetic mean of four numbers.*/
+// /*4.Write a program that calculates an arithmetic mean of four numbers.*/
 
-var avvar="";
-function ariTM(arr){
-    var sum = 0;
-    for(var i=1; 1<arr.length; i++){
-        sum = sum + arr[i];
-        avvar = sum / arr.length;
-        return sum;
-    }
-    
-}
+// var avva = "";
+// function ariTM(arr) {
+//     var sum = 0;
+//     for (var i = 1; 1 < arr.length; i++) {
+//         sum = sum + arr[i];
+//         avva = sum / arr.length;
+//         return sum;
+//     }
 
-console.log(ariTM([3,4,5,6]));
+// }
 
-/*7.Write a program that calculates a number of digits of a given number. */
+// console.log(ariTM([3, 4, 5, 6]));
 
-/*9.Write a program that calculates the sum of odd elements of a given array.*/
-function sumNum (arr){
-    var a = 0;
-    for(var i = 0; i<arr.length; i++){
-        if(i%2===1){
-            a+=arr[i];
-           
-        }
-    } return a;
-}
+// /*7.Write a program that calculates a number of digits of a given number. */
 
-console.log (sumNum([2,3,1,4]));
 
-/*10.Write a program that calculates a number of appearances of a given number in a given array.*/
 
-function countAp (a, e){
-    var b= 0;
-    for(var i = 0; i<a.length; i++){
-if(e===a[i]){
-    b++;
-   
-}
-return b;
+// /*9.Write a program that calculates the sum of odd elements of a given array.*/
+// function sumNum(arr) {
+//     var a = 0;
+//     for (var i = 0; i < arr.length; i++) {
+//         if (i % 2 === 1) {
+//             a += arr[i];
 
-    }
-}
-console.log(countAp([2, 4, 7, 8, 7, 7, 1], 7));
+//         }
+//     } return a;
+// }
+
+// console.log(sumNum([2, 3, 1, 4]));
+
+// /*10.Write a program that calculates a number of appearances of a given number in a given array.*/
+
+// function countAp(a, e) {
+//     var b = 0;
+//     for (var i = 0; i < a.length; i++) {
+//         if (e === a[i]) {
+//             b++;
+
+//         }
+//         return b;
+
+//     }
+// }
+// console.log(countAp([2, 4, 7, 8, 7, 7, 1], 7));
