f7a2d0f0d4707ba5d292d7721394e45a5f930a65
“vukas86” <vukasbit@gmail.com>
2021-05-10 23:08:15 +0200

    functions exercise part 3

---
+++ functions3.js
@@ -0,0 +1,31 @@
+/* 1.Write a program to insert a string within a string at a particular position (default is 1, beginning of a string). */
+/* var a = "My random string";
+
+function strng(str){
+    
+} */
+
+/* 2.Write a program to join all elements of the array into a string skipping elements that are undefined, null, NaN or Infinity */
+
+/* function filterStrng(strng){
+    var a =[];
+    for(var i=0; i<strng.length; i++){
+        if(strng[i] === undefined || strng[i] === null || strng[i] === NaN || strng[i]==="infinity" ){
+            a.push(strng[i]);
+            
+        }
+    }return a;
+}
+
+console.log(filterStrng([NaN, 0, 15, false, -22, '', undefined, 47, null])); */
+
+function buildRow(num){
+    var row="";
+
+    for(var i=0; i<num; i++){
+        row+="* ";
+    }
+    return row;
+}
+
+console.log(buildRow(3));
