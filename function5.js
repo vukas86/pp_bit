e5642c6a4db210cca92241d26a174e2de5e2c806
“BIT <vukasbit@gmail.com>
2021-05-14 13:00:56 +0200

    functions 5

--- function5.js
+++ function5.js
@@ -1,2 +1,5 @@
 /* 1. Find the min and max element in the following array and switch their places.Print out the
 modified array in the console. */
+/*
+2.Use the following array to make a new one by dividing its values by two and adding 5. If
+a given element&#39;s value is 0, change it to 20. */
